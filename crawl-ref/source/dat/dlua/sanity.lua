80b28ef3d489c1c27d787b88f2b18946bd18c8fc
test <asdf@asdf.com>
2026-02-08 11:29:32 -0600

    restore assert

--- crawl-ref/source/dat/dlua/sanity.lua
+++ crawl-ref/source/dat/dlua/sanity.lua
@@ -1,9 +1,7 @@
 -- Sanity checks that should be run just before the game starts.
 
 local function assert_place_has_map(place)
-  if not dgn.map_by_place(place) then
-    crawl.mpr("Warning: no map found for " .. place)
-  end
+  assert(dgn.map_by_place(place), "No map found for " .. place)
 end
 
 local function sanity_checks()
